c19eb9c4de56aa99a2170aa6cdedeb6cfbb0229a
Tejaswita Wakhure <31151862+TejaswitaW@users.noreply.github.com>
2023-07-28 18:55:24 +0530

    page counter implemented using session in django

--- pagecount/views.py
+++ pagecount/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    cnt = request.session.get('count',0)
+    newcnt = cnt + 1 
+    request.session['count'] = newcnt
+    return render(request,'pagecount/home.html',{'c':newcnt})
